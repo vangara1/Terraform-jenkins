0e8204600b28e3de65c4e74b65db7eb6b99534f6
sandy <101511412+vangara1@users.noreply.github.com>
2023-01-06 16:56:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-      bucket = "devops-tf-talent"
+      bucket = "rajeshalli-devops"
       dynamodb_table = "terraform-state-lock-dynamo"
       key    = "devops10/terraform.tfstate"
       region = "us-east-1"
