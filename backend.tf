1ec8a71bb2f0c4e45f6830bb3c8859ef4aa78ae0
sandy <101511412+vangara1@users.noreply.github.com>
2023-01-06 17:33:07 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-      bucket = "rajeshalli-devops"
-      dynamodb_table = "terraform-state-lock-dynamo"
-      key    = "devops10/terraform.tfstate"
-      region = "us-east-1"
-    }
-  }
-
